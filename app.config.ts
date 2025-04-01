e2fec7bce42b213ef7340e6ee424247d3395867a
Cascade <hawktuahskibidisigma69420@gmail.com>
2025-04-01 10:31:57 -0400

    e

--- app.config.ts
+++ app.config.ts
@@ -15,7 +15,7 @@ export default defineAppConfig({
       title: 'Cascade',
       links: [
         { name: 'Home', href: '/' },
-        { name: 'Games', href: '/games' }
+        { name: 'Games', href: '1.games/games' }
       ]
     },
     footer: {
